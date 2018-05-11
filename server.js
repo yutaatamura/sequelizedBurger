2409092c698e81e0ba8160324e5f4fcafda0b95e
Yuta <yutaatamura@gmail.com>
2018-05-11 14:58:01 -0500

    Working delete functionality

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var methodOverride = require('method-override');
 
 var app = express(); 
 
@@ -12,6 +13,8 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
+
+app.use(methodOverride('_method'));
 
 var exphbs = require("express-handlebars");
 
